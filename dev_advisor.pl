fe84208dea2ca3de5b0fd7fbfbec3e32be40c80f
eddy <mwendaeddy@gmail.com>
2019-03-31 13:09:39 +0300

    Improve UI flow and add some more knowledge

--- dev_advisor.pl
+++ dev_advisor.pl
@@ -31,24 +31,36 @@ optimized_runtime_environment(jvm, java).
 % mobile app facts
 mobile_app_os(android).
 mobile_app_os(iOS).
+
+tools_for_mobile_os(android, 'Android Studio IDE').
+tools_for_mobile_os(android, 'Java programming language').
+tools_for_mobile_os(android, 'Kotlin programming language').
+
+tools_for_mobile_os(ios, 'Visual Studio IDE').
+tools_for_mobile_os(ios, 'Swift programming language').
+
+tools_for_mobile_os(both, 'Xamarin Framework which uses C# programming language').
+tools_for_mobile_os(both, 'Flutter Framework which uses Dart programming language').
+
 mobile_app_framework(xamarin).
 mobile_app_framework(flutter).
 mobile_app_language('C#', xamarin).
 mobile_app_language(dart, flutter).
 mobile_app_ide(xamarin_studio, xamarin).
 mobile_app_ide(android_studio_ide, flutter).
-mobile_app_db(sqlite)
+mobile_app_db(sqlite).
 
 
-%desktop app facts
+% desktop app facts
 desktop_app_language(java).
 desktop_app_language('C#').
+desktop_app_language('C++').
 desktop_app_language(python).
 desktop_app_ide(java,netbeans).
-desktop_app_ide(java, eclipse_IDE).
-desktop_app_ide('C#', visual_studio_IDE).
-desktop_app_ide('C++', visual_studio_IDE).
-desktop_app_ide(python, pycharm_IDE).
+desktop_app_ide(java, eclipse).
+desktop_app_ide('C#', 'Visual Studio').
+desktop_app_ide('C++', 'Visual Studio').
+desktop_app_ide(python, 'PyCharm').
 
 % database facts
 database(mysql).
@@ -72,55 +84,103 @@ standalone.
 web_app :- client_side, server_side. 
 
 %UI
-check_web_app :- write('Does your application have a client side?'), nl, 
-				 read(Client_side), Client_side=yes, nl, nl, 
-				 write('Does your application have a server side?'), nl, 
-				 read(Server_side), Server_side=yes, nl, 
-				 write('Your application is a web application!'), nl.
 
-check_web_app :- nl, write('Sorry, your application is not a web application. Check if it is a stand alone.'),nl.
+% Determine kind of application
+find_out_software :-
+	write("Do you want to build a Web application? (y/n)"), nl,
+	read(X), nl,
+	X=y, building_web_app;
 
-mobile_app_os :- nl, write('MObile Applications runs on two platforms mainly '), nl, forall(mobile_app_os(X), writeln((X))).
+	write("Do you want to build a Mobile application? (y/n)"), nl,
+	read(X), nl,
+	X=y, building_mobile_app;
 
-mobile_apps :- nl, write('Mobile Applications can be:'), nl, 
-				write('1. Stand Alone'), nl, 
-				write('2. Clients'), nl,
-				write('Which one do you wish to explore?'), nl,
-				read(Choice), nl, app_type(Choice).
-				app_type(Choice) :- Choice=:=1, nl, write('These apps have everything bundled within them. They do not require constant network connectivity to download and process the data.'), nl.
-				app_type(Choice) :- Choice=:=2, nl, write('These apps act as a client to provide a front-end to another application running on a server somewhere.'), nl.
+	write("Do you want to build a Desktop application? (y/n)"), nl,
+	read(X), nl,
+	X=y, building_desktop_app;
 
-mobile_app_tools :- nl, write('Explore the set of tools to build a mobile application. There are: '), nl,
-						write('1. Frameworks'), nl, 
-						write('2. IDE\'s'), nl, 
-						write('3. Languages'), nl,
-						read(Choice), nl, tool_type(Choice).
-						tool_type(Choice) :- Choice=:=1, nl, write('Here is a list of mobile frameworks'), nl, forall(mobile_app_framework(X), writeln((X))).
-						tool_type(Choice) :- Choice=:=2, nl, write('Here is a list of IDE\'s'), nl, forall(mobile_app_ide(X, Y), writeln((X, Y))).
-						tool_type(Choice) :- Choice=:=3, nl, write('Here is a list of Languages'), nl, forall(mobile_app_language(X, Y), writeln((X, Y))).
-
-mobile_app_database :- nl, write('Mobile Applications strictly use SQLite database.'),nl.
-
-desktop_app_tools :- nl, write('Explore the set of tools to build a desktop application. There are: '), nl, 
-						write('1. Languages'), nl, 
-						write('2. IDE\'s'), nl,
-						read(Choice), nl, desktop_tool_type(Choice).
-						desktop_tool_type(Choice) :- Choice=:=1, nl, write('Here is a list of Languages'), nl, forall(desktop_app_language(X), writeln((X))).
-						desktop_tool_type(Choice) :- Choice=:=2, nl, write('Here is a list of IDE\'s'), nl, forall(desktop_app_ide(X, Y), writeln((X, Y))).
-
-popular_databases :- nl, write('The most common databases include: '), nl, forall(database(X), writeln((X))), nl, 
-						write('Databases use Object Relation Mapping(ORMs) to make coding easer. Here is a list of popular languages and their ORMs'), nl, forall(orm(X,Y), writeln((X,Y))).
+	write(">>> Sorry, cannot determine which classification your application falls under.").
 
 
+building_web_app :-
+	write("Which side are you building?"), nl,
+	write("1. Client-side(default)"), nl,
+	write("2. Server-side"), nl,
+	write("3. Full-stack"), nl,
+
+	read(C), web_app_side_choice(C).
+
+	web_app_side_choice(C) :-
+		C=:=3, write(">>> For full-stack use "), nl,
+		list_client_side_languages,
+		list_server_side_languages.
+
+	web_app_side_choice(C) :-
+		C=:=2, write(">>> For server-side use:"), nl,
+		list_server_side_languages.
+
+	web_app_side_choice(_C) :-
+		list_client_side_languages.
 
 
+list_server_side_languages:-
+	forall(server_side_language(L), format("* ~t~w language,~n", L)),
+	write(">>> Frameworks make development easier, hence use:"), nl,
+	forall(server_side_framework(F, L),
+	format("* ~t~s framework for ~s language~n", [F, L])).
 
 
+list_client_side_languages:-
+	write(">>> For client-side use:"), nl,
+	forall(client_side_language(L), format("* ~t~w language,~n", L)),
+	write(">>> Frameworks make development easier, hence use:~n"), nl,
+	forall(client_side_framework(F, L),
+	format("* ~t~s framework for ~s language~n", [F, L])).
 
 
+building_mobile_app :-
+	nl,
+	write("Should it use a database? (y/n)"), nl,
+	read(X), db_choice(X).
+	db_choice(X) :- X=y, mobile_app_with_db.
+	db_choice(_X) :- mobile_app_os.
 
 
-				 
+building_desktop_app :-
+	nl,
+	write("Desktop apps can be built using:"), nl,
+	forall(desktop_app_language(Z), format("* ~w\n", Z)),
+	write("Which langauge would you want to use?"), nl,
+	read(C), desktop_language_choice(C).
+	
+	desktop_language_choice(C) :-
+		desktop_app_ide(C, IDE),
+		format(">>> For ~w use ~w IDE", [C, IDE]).
+	desktop_language_choice(_C) :- write("Could not determine which IDE to use for that language"), nl.
+
+
+mobile_app_with_db :-
+	write(">>> You should use ["),
+	forall(mobile_app_db(Z), write(Z)),
+	write("] as possible databases."), nl,
+	write(">>> This is because mobile phones have smaller memory hence using a light-weight database is preferrable"), nl,
+	mobile_app_os.
+
+
+mobile_app_os :-
+	nl,
+	write("Which Mobile OS are you building an app for?"), nl,
+	write("1. Android (default)"), nl,
+	write("2. iOS"), nl,
+	write("3. Both"), nl,
+	read(C), os_choice(C).
+	os_choice(C) :- C=:=3, write(">>> For both iOS and Android use "),
+					forall(tools_for_mobile_os(both, Z), format("{~w} or ", Z)).
+	os_choice(C) :- C=:=2, write(">>> For iOS use "),
+					forall(tools_for_mobile_os(ios, Z), format("~w, ", Z)).
+	os_choice(_C) :- write(">>> For Android use "),
+					forall(tools_for_mobile_os(android, Z), format("~w, ", Z)).
+		 
 
 
 
